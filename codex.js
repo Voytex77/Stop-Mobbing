9eca095723e218f21646ff6bfbdae4808a945cab
Voytex77 <voytex77@op.pl>
2025-10-28 12:56:58 +0100

    Basic Animations done

---
+++ codex.js
@@ -0,0 +1,13 @@
+const carouselElements = 
+[
+    `
+        <h2>Ochrona przed niekorzystnym traktowaniem</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, at deleniti assumenda suscipit, quibusdam amet vel possimus esse porro cupiditate reprehenderit error ex nulla eaque atque illo? Laboriosam, earum nobis?</p>
+    `,
+    `
+        <h2>Artykuł nr 2</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, at deleniti assumenda suscipit, quibusdam amet vel possimus esse porro cupiditate reprehenderit error ex nulla eaque atque illo? Laboriosam, earum nobis?</p>
+    `,
+]
+
+document.getElementById("codex-article")
